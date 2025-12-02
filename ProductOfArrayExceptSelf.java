eeaf66e01b81df1a6b8d560e10bde649f93aed16
kiran <kiranavula564@gmail.com>
2025-12-02 10:47:13 +0530

    product array except it self

---
+++ ProductOfArrayExceptSelf.java
@@ -0,0 +1,36 @@
+public class ProductOfArrayExceptSelf {
+    static int[] ProductOfArrayExceptSelf(int[] a){
+        int[] prefix=new int[a.length];
+        int[] suffix=new int[a.length];
+        int sum=1;
+        for(int i=0;i<a.length;i++){
+            sum*=a[i];
+            prefix[i]=sum;
+        }
+        sum=1;
+        for(int i=a.length-1;i>=0;i--){
+            sum*=a[i];
+            suffix[i]=sum;
+        }
+
+        for(int i=0;i<a.length;i++){
+            if(i==0||i==a.length-1){
+                if(i==0){
+                    a[i]=suffix[1];
+                }else a[i]=prefix[a.length-2];
+            }else
+            a[i]=prefix[i-1]*suffix[i+1];
+        }
+        return a;
+    }
+
+    public static void main(String[] arg){
+        int[] a={1,2,3,4};
+        a=ProductOfArrayExceptSelf(a);
+        for(int i:a){
+            System.out.print(i+" ");
+        }
+        
+    }
+    
+}
